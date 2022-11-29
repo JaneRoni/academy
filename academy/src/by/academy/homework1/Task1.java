febc3198bd8786afdf6d340f0b1027d8959ad5ac
Jane <janeroni93@gmail.com>
2022-11-29 16:54:42 +0300

    Home

--- academy/src/by/academy/homework1/Task1.java
+++ academy/src/by/academy/homework1/Task1.java
@@ -27,9 +27,25 @@ public class Task1 {
 		if (summa < 100) { //Сумма до 100 рублей -> 5%
 			double skidka = (5*summa)/100;
 			System.out.println ("Сумма = " + (summa-skidka));
-		}else {
-				
+		}else{
+			if (summa >= 100 && summa < 200) { //Сумма от 100 рублей включая до 200 рублей не включая -> 7%
+				double skidka = (7*summa)/100;
+				System.out.println ("Сумма = " + (summa-skidka));
+			}else{
+				if (summa >= 200 && summa < 300) { //от 200 рублей включая до 300 рублей не включая -> 12%
+					double skidka = (12*summa)/100;
+					System.out.println ("Сумма = " + (summa-skidka));
+				}else {
+					if (summa >= 300 && summa < 400) { //4) Сумма от 300 рублей включая до 400 рублей не включая -> 15%
+						double skidka = (15*summa)/100;
+						System.out.println ("Сумма = " + (summa-skidka));
+					}else {
+						double skidka = (20*summa)/100; //5) Сумма покупки больше 400 рублей включая -> 20%
+						System.out.println ("Сумма = " + (summa-skidka));
+					}
+				}
 			}
-		
 		}
+		
+	}
 }
--- academy/src/by/academy/homework1/Task3.java
+++ academy/src/by/academy/homework1/Task3.java
@@ -1,7 +1,17 @@
 package by.academy.homework1;
 
+import java.util.Scanner;
+
 /*Ввести с консоли число от 1 до 10, вывести на экран таблицу умножения для этого числа. (10 чисел).*/
 
 public class Task3 {
+	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Введите число от 1 до 10: ");
+		//String str = scan.nextLine();
+		scan.close();
+		
+		System.out.println
 
 }
