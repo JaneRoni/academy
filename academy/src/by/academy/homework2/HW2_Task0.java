169522ca0a09d8c8f420c8bcc470483c637d9f02
Jane <janeroni93@gmail.com>
2022-12-27 14:20:38 +0300

    2/0

--- academy/src/by/academy/homework2/HW2_Task0.java
+++ academy/src/by/academy/homework2/HW2_Task0.java
@@ -1,40 +1,48 @@
 package by.academy.homework2;
+
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HW2_Task0 {
 
-	public class Solution {
+	static int pairs(int k, int[] arr) {
+		int pairsCount = 0;
 
-		// Complete the pairs function below.
-		static int pairs(int k, int[] arr) {
-			int pairsCount = 0;
-			int arr2 [] = new int [10];
-			return pairsCount;
-		}
-
-		private static final Scanner scanner = new Scanner(System.in);
-
-		public static void main(String[] args) throws IOException {
-
-			String[] nk = scanner.nextLine().split(" ");
-
-			int n = Integer.parseInt(nk[0]);
-
-			int k = Integer.parseInt(nk[1]);
-
-			int[] arr = new int[n];
-
-			String[] arrItems = scanner.nextLine().split(" ");
-
-			for (int i = 0; i < n; i++) {
-				arr[i] = Integer.parseInt(arrItems[i]);
-				System.out.println(arr[i]);
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i+1; j < arr.length; j++) {
+				if (arr[i] - arr[j] == k || arr[j] - arr[i] == k) {
+					pairsCount++;
+					//System.out.println(pairsCount);
+				}
 			}
 
-			int result = pairs(k, arr);
-			System.out.println(result);
+		}
+		return pairsCount;
+	}
 
-			scanner.close();
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException{
+
+		System.out.println("Введите значения n и k: ");
+		String[] nk = scanner.nextLine().split(" ");
+		System.out.println(Arrays.toString(nk));
+
+		int n = Integer.parseInt(nk[0]);
+		int k = Integer.parseInt(nk[1]);
+
+		int[] arr = new int[n];
+		String[] arrItems = scanner.nextLine().split(" ");
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(arrItems[i]);
+			System.out.println(arr[i]);
 		}
 
+		int result = pairs(k, arr);
+		System.out.println(result);
+
+		scanner.close();
+	}
 }
