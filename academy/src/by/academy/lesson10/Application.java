7cfead263a4d0f04b3b2cfb69b6d6271aeead9da
Jane <janeroni93@gmail.com>
2022-12-27 16:45:59 +0300

    L10

--- academy/src/by/academy/lesson10/Application.java
+++ academy/src/by/academy/lesson10/Application.java
@@ -1,19 +1,19 @@
-package by.academy.lesson10;
-
-import java.awt.print.Printable;
-
-public class Application {
-	public static void main(String... args) {
-		Printable [] array = new Printable[5];
-		array[0] = new Book ("Kolobok1");
-		array[1] = new Book ("Kolobok2");
-		array[2] = new Book ("Kolobok3");
-		array[3] = new Magazin ("Kolobok4",2000);
-		array[4] = new Magazin ("Kolobok5",2010);
-		
-		for (Printable p : array) {
-			p.print();
-		}
-	}
-
-}
+//package by.academy.lesson10;
+//
+//import java.awt.print.Printable;
+//
+//public class Application {
+//	public static void main(String... args) {
+//		Printable [] array = new Printable[5];
+//		array[0] = new Book ("Kolobok1");
+//		array[1] = new Book ("Kolobok2");
+//		array[2] = new Book ("Kolobok3");
+//		array[3] = new Magazin ("Kolobok4",2000);
+//		array[4] = new Magazin ("Kolobok5",2010);
+//		
+//		for (Printable p : array) {
+//			p.print();
+//		}
+//	}
+//
+//}
